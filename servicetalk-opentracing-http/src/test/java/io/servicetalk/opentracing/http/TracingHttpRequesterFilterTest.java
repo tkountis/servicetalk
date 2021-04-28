72ceb9d01169960e3eed9a6bd4774f84acccc7b1
Thomas Kountis <thomas_kountis@apple.com>
2021-04-28 12:55:20 +0100

    Respect missing B3 sampled headers (#1512)
    * Respect missing B3 sampled headers

--- servicetalk-opentracing-http/src/test/java/io/servicetalk/opentracing/http/TracingHttpRequesterFilterTest.java
+++ servicetalk-opentracing-http/src/test/java/io/servicetalk/opentracing/http/TracingHttpRequesterFilterTest.java
@@ -137,7 +137,7 @@ class TracingHttpRequesterFilterTest {
                 assertFalse(lastFinishedSpan.tags().containsKey(ERROR.getKey()));
 
                 verifyTraceIdPresentInLogs(stableAccumulated(1000), requestUrl, serverSpanState.traceId,
-                        serverSpanState.spanId, serverSpanState.parentSpanId, TRACING_TEST_LOG_LINE_PREFIX);
+                        serverSpanState.spanId, null, TRACING_TEST_LOG_LINE_PREFIX);
             }
         }
     }
@@ -162,8 +162,10 @@ class TracingHttpRequesterFilterTest {
                         assertThat(serverSpanState.spanId, isHexId());
                         assertThat(serverSpanState.parentSpanId, isHexId());
 
-                        assertThat(serverSpanState.traceId, equalToIgnoringCase(clientSpan.traceIdHex()));
-                        assertThat(serverSpanState.parentSpanId, equalToIgnoringCase(clientSpan.spanIdHex()));
+                        assertThat(serverSpanState.traceId, equalToIgnoringCase(
+                                clientSpan.context().traceState().traceIdHex()));
+                        assertThat(serverSpanState.parentSpanId, equalToIgnoringCase(
+                                clientSpan.context().traceState().spanIdHex()));
 
                         // don't mess with caller span state
                         assertEquals(clientSpan, tracer.activeSpan());
